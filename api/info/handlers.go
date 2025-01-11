f43057f0e2d0b8c003eaead6a915c2b76b71ad9e
Tony Stark <ironman0x7b2@protonmail.com>
2025-01-11 16:50:25 +0530

    feat: implement get info api handler

--- api/info/handlers.go
+++ api/info/handlers.go
@@ -1,13 +1,41 @@
 package info
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 
-	nodecontext "github.com/sentinel-official/dvpn-node/context"
+	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/gin-gonic/gin"
+	"github.com/sentinel-official/sentinel-go-sdk/libs/geoip"
+	"github.com/sentinel-official/sentinel-go-sdk/types"
+
+	"github.com/sentinel-official/dvpn-node/context"
 )
 
-func HandlerGetInfo(ctx *nodecontext.Context) gin.HandlerFunc {
-	return func(c *gin.Context) {
+// HandlerGetInfo returns a handler function to retrieve node information.
+func HandlerGetInfo(c *context.Context) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		downLink, upLink := c.SpeedtestResults()
+		loc := c.Location()
 
+		// Construct the result structure with node information.
+		res := ResultGetInfo{
+			Addr:         c.NodeAddr().String(),
+			DownLink:     downLink.String(),
+			HandshakeDNS: false,
+			Location: &geoip.Location{
+				City:      loc.City,
+				Country:   loc.Country,
+				Latitude:  loc.Latitude,
+				Longitude: loc.Longitude,
+			},
+			Moniker: c.Moniker(),
+			Peers:   c.Service().PeerCount(),
+			Type:    c.Service().Type().String(),
+			UpLink:  upLink.String(),
+			Version: version.Version,
+		}
+
+		// Send the result as a JSON response with HTTP status 200.
+		ctx.JSON(http.StatusOK, types.NewResponseResult(res))
 	}
 }
--- api/info/routes.go
+++ api/info/routes.go
@@ -3,9 +3,9 @@ package info
 import (
 	"github.com/gin-gonic/gin"
 
-	nodecontext "github.com/sentinel-official/dvpn-node/context"
+	"github.com/sentinel-official/dvpn-node/context"
 )
 
-func RegisterRoutes(ctx *nodecontext.Context, r gin.IRouter) {
+func RegisterRoutes(ctx *context.Context, r gin.IRouter) {
 	r.GET("/", HandlerGetInfo(ctx))
 }
