b6daf32d8e630dae07dfde5500a0cbbdf42b89f8
Tony Stark <ironman0x7b2@protonmail.com>
2025-01-28 14:36:22 +0530

    feat: add tls related config for the node

--- utils/http.go
+++ utils/http.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/rand"
 	"crypto/tls"
+	"fmt"
 	"net"
 	"net/http"
 
@@ -14,44 +15,47 @@ func ListenAndServeTLS(addr, certFile, keyFile string, handler http.Handler) err
 	// Load the TLS certificate and key
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to load tls certificate: %w", err)
 	}
 
 	// Create a TCP listener
-	l, err := net.Listen("tcp", addr)
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to listen: %w", err)
 	}
 
 	// Create a cmux multiplexer
-	mux := cmux.New(l)
+	mux := cmux.New(listener)
 
-	// Create matchers for TLS and non-TLS traffic
+	// Define matchers for TLS and non-TLS traffic
 	tlsMux := mux.Match(cmux.TLS())
 	anyMux := mux.Match(cmux.Any())
 
+	// Reuse the TLS configuration
+	cfg := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		Rand:         rand.Reader,
+	}
+
 	// Serve TLS traffic
 	go func() {
-		err := http.Serve(
-			tls.NewListener(tlsMux, &tls.Config{
-				Certificates: []tls.Certificate{cert},
-				Rand:         rand.Reader,
-			}),
-			handler,
-		)
-		if err != nil {
-			panic(err)
+		tlsMux := tls.NewListener(tlsMux, cfg)
+		if err := http.Serve(tlsMux, handler); err != nil {
+			panic(fmt.Errorf("failed to serve tls: %w", err))
 		}
 	}()
 
 	// Serve non-TLS traffic
 	go func() {
-		err := http.Serve(anyMux, handler)
-		if err != nil {
-			panic(err)
+		if err := http.Serve(anyMux, handler); err != nil {
+			panic(fmt.Errorf("failed to serve any: %w", err))
 		}
 	}()
 
-	// Start the mux server
-	return mux.Serve()
+	// Start the multiplexer
+	if err := mux.Serve(); err != nil {
+		return fmt.Errorf("failed to serve: %w", err)
+	}
+
+	return nil
 }
